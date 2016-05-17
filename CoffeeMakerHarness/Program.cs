a4757a75619c40e22c33c90776f6b0b10123676a
dsolovay <dan.solovay@velir.com>
2016-05-17 01:26:06 -0400

    Add console harness

---
+++ CoffeeMakerHarness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMakerHarness
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("(F)ill Resevoir, Press (B)utton, (R)emove Pot, (P)lace Pot, (E)mpty Pot, E(X)it");
+
+				string line = Console.ReadLine();
+
+				string first = line.ToUpper().Substring(0, 1);
+
+				if (first == "X")
+				{
+					Console.WriteLine("Goodbye");
+					Console.ReadLine();
+					return;
+				}
+			}
+		}
+	}
+}
